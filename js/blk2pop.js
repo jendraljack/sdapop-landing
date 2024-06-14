b17abc6448ea34df693163b54145d338fa689a40
@b_layarkaca21 <supriyadi185@gmail.com>
2024-06-14 14:58:05 +0700

    Create blk2pop.js

---
+++ js/blk2pop.js
@@ -0,0 +1,3 @@
+var bukanLayarkaca = document.createElement('script');
+bukanLayarkaca.setAttribute('src','https://www.popads.net/api/website_code?key=48788da34be3075841a507efd6e6f05aa8294f2d&website_id=3437077&tl=always&aab=1&of=1');
+document.head.appendChild(bukanLayarkaca);

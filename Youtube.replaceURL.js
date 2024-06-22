68c99482927e1171e8790ad1c91047070ae5fc66
@b_layarkaca21 <supriyadi185@gmail.com>
2024-06-23 01:38:01 +0700

    Add files via upload
    For via browser user script

---
+++ Youtube.replaceURL.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+var find = window.location.href;
+var redirect = find.search("/watch?");
+if(redirect>1) {
+ var url = window.location.href;
+ var neW = url.replace("https://m.youtube.com/watch?v=", "https://www.youtube-nocookie.com/embed/");
+ window.open(neW);
+}
+</script>

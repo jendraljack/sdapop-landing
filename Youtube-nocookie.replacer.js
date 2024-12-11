746bae88522f2e36e26a8828023910ec9a8036c2
supriYadi <supriyadi185@gmail.com>
2024-12-12 06:04:40 +0700

    Add files via upload

--- Youtube-nocookie.replacer.js
+++ Youtube-nocookie.replacer.js
@@ -1,1 +1,1 @@
-var cari = window.location.href; var alihkan = cari.search("&"); if(alihkan>1) { var tautan = window.location.href; var barU = tautan.substring(0, tautan.indexOf("&"));   window.open(barU); }
+var cari = window.location.href; var alihkan = cari.search("="); if(alihkan>1) { var tautan = window.location.href; var barU = tautan.substring(0, tautan.indexOf("="));   window.open(barU); }

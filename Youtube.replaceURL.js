6561510bc9161b8dc0a7ebc3f0397d400e36b896
supriYadi <supriyadi185@gmail.com>
2024-12-14 23:08:50 +0700

    Update Youtube.replaceURL.js

--- Youtube.replaceURL.js
+++ Youtube.replaceURL.js
@@ -3,5 +3,5 @@ var redirect = find.search("watch");
 if(redirect>1) {
  var url = window.location.href;
  var neW = url.replace("https://m.youtube.com/watch?v=", "https://www.youtube-nocookie.com/embed/");
- window.open(neW);
+ window.location.href=neW;
 }
